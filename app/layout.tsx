b3940884a903d600e2e67cb0f9e3f67f05260588
Chris Kehoe <chriskehoe@Chriss-MacBook-Pro.local>
2025-02-14 11:22:54 -0700

    Metadata SEO change

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,11 @@ import { Analytics } from '@vercel/analytics/react';
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CSE 360 RAG Chatbot",
-  description: "An AI teaching assistant that helps CSE 360 students by providing accurate answers from past Ed Discussion posts.",
+  title: "CSE 360 Chatbot",
+  description: "An AI teaching assistant that helps CSE 360 students by providing accurate answers from past Ed Discussion posts and Course Documents.",
   openGraph: {
-    title: "CSE 360 RAG Chatbot",
-    description: "An AI teaching assistant that helps CSE 360 students by providing accurate answers from past Ed Discussion posts.",
+    title: "CSE 360 Chatbot",
+    description: "An AI teaching assistant that helps CSE 360 students by providing accurate answers from past Ed Discussion posts and Course Documents.",
     type: "website",
   },
   twitter: {
